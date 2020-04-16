a92fb63cefdff98199b89e81c0d5433571342cf7
Dastan Abeuov <d.abeuov9@@gmail.com>
2020-04-17 03:17:17 +0600

    failure

--- app/models/xcategory.rb
+++ app/models/xcategory.rb
@@ -1,8 +1,7 @@
 class Xcategory < ApplicationRecord
-  attr_accessor :name, :description, :synonym, :code, :version_date, :publish, :user_id, :xroot_id
-
+  #attr_accessor :name, :description, :synonym, :code, :version_date, :publish, :user_id
   belongs_to :user
   belongs_to :xroot
 
-  has_many :xclasses, dependent: :destroy  
+  has_many :xclasses, dependent: :destroy
 end
