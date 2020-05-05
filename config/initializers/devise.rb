ebab8ea503b36fdd274216ebe3e3740854aa5df8
Dastan Abeuov <d.abeuov9@@gmail.com>
2020-05-06 05:26:23 +0600

    fix errror

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'e775bbaa75cb98a7602d1ebca71a6f4accf1e0276be2d751026355a8f648f2f171f38ca776b1f5eb9622f54a07b7cd36497620596964d9257fb86c6f94ab158e'
-  config.secret_key = 'e71b16281c5e7d7dcf3da5ab11da694c3fd3f91f684421eea5ddc0da4d85de0efa3e9bcb54c3eeb69218b9d68728e9d03446a6e0a8d6bd18b4da7fb945e4121d'
+  
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
