6472c63a83aa509423192dcdf0475b42e05a9970
Dastan Abeuov <d.abeuov9@@gmail.com>
2020-05-10 18:16:17 +0600

    by bag heroku

--- config/initializers/rspec_api_documentation.rb
+++ config/initializers/rspec_api_documentation.rb
@@ -1,7 +1,7 @@
 RspecApiDocumentation.configure do |config|
   config.docs_dir = Rails.root.join('public/docs')
   config.format = [:open_api]
-  config.curl_host = 'http://demo-classifier.herokuapp.com'
+  config.curl_host = 'http://localhost:300'#demo-classifier.herokuapp.com'
   config.api_name = 'Classifier App API'
   config.api_explanation = "API Clasifier Description"
   config.request_headers_to_include = []
