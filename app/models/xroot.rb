8292a591a154825d13b409a3a54c07b243a06353
Dastan Abeuov <d.abeuov9@@gmail.com>
2020-05-13 02:19:53 +0600

    finish xroot test and delete before action

--- app/models/xroot.rb
+++ app/models/xroot.rb
@@ -4,7 +4,7 @@ class Xroot < ApplicationRecord
   has_many :xcategories, dependent: :destroy
   has_many :properties, as: :propertyable, :inverse_of => :propertyable
 
-  #validates :name, presence: true
+  validates :name, presence: true
   
   translates :name, :description, :synonym, :code, :version_date, :publish, :user_id
   accepts_nested_attributes_for :translations, allow_destroy: true
