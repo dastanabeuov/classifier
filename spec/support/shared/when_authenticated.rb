c997165a81c90038882d7b89e3316113ec62405f
dastanabeuov <d.abeuov9@gmail.com>
2021-11-25 11:24:33 +0600

    remove file spec/support/shared/when_authenticated.rb

--- spec/support/shared/when_authenticated.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.shared_context 'when authenticated' do
-  security [bearer: []]
-
-  let(:user) { create(:user) }
-  let(:access_token) { create(:access_token, resource_owner_id: user.id) }
-  let(:Authorization) { "Bearer #{access_token.token}" }
-end
